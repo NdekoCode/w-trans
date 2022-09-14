f29dd659e47fb40a48cf6ff25f184c5ce04b34f8
NdekoCode <arickbulakali@gmail.com>
2022-09-14 17:23:56 +0200

    install React router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import routes from "./routes/routes";
 
 function App() {
   return (
     <div className="App">
+      <Routes></Routes>
       <Navbar />
       <Footer />
     </div>
