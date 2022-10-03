8cc5695c614c0fea88335fa2f139b439b447f54a
NdekoCode <arickbulakali@gmail.com>
2022-10-03 23:20:40 +0200

    add OurProject component

---
+++ src/components/OurProject.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OurProject = () => {
+  return (
+    <section id="projects" className="px-3 py-20 md:lg:xl:px-40 bg-opacity-10 ">
+      <h2 className="title title-2 max-w-md">Nos derniers projets</h2>
+    </section>
+  );
+};
+
+export default OurProject;
